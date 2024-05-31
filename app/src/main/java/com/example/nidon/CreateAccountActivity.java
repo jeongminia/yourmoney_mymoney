2863a81cde0c82df2d64d66f3c073fe833e43409
jayinsummer <112365313+jayinsummer@users.noreply.github.com>
2024-05-31 22:28:15 +0900

    Initial commit

---
+++ app/src/main/java/com/example/nidon/CreateAccountActivity.java
@@ -0,0 +1,31 @@
+package com.example.nidon;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_account);
+
+        EditText accountNameEditText = findViewById(R.id.account_name);
+        Button saveButton = findViewById(R.id.save_button);
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String accountName = accountNameEditText.getText().toString();
+                Intent resultIntent = new Intent();
+                resultIntent.putExtra("ACCOUNT_NAME", accountName);
+                setResult(RESULT_OK, resultIntent);
+                finish();
+            }
+        });
+    }
+}
