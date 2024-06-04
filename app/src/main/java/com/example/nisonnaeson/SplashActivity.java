a5a13eb70f94691e0c8ba2694d98ee9d5dc43492
hoyeon <fnfn0901@sookmyung.ac.kr>
2024-06-04 10:30:56 +0900

    스플래시 화면 추가 상단 상태바 색상 변경

---
+++ app/src/main/java/com/example/nisonnaeson/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.example.nisonnaeson;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        // 3초 후 LoginActivity로 이동
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(intent);
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out); // 전환 애니메이션 추가
+                finish(); // 현재 액티비티 종료
+            }
+        }, 3000); // 3000 milliseconds = 3 seconds
+    }
+}
